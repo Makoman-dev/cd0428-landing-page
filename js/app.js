ae0943c48a87262b04f2cae7ff0df5041430764d
Makoman <elgamlahmed@gmail.com>
2024-10-23 12:34:56 +0300

    adding active state slates

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const navigation = document.getElementById("navbar__menu");
 const modalBtn = document.getElementById("modal-btn");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close-btn");
+const sections = document.querySelectorAll('.section');
 
 // created the objects to be used for each section obtion
 const navigationData = [
@@ -62,3 +63,35 @@ for (const item of navigationData) {
   listItem.append(link);
   navList.appendChild(listItem);
 }
+
+let sectionInViewport = function(section) {
+  let distance = section.getBoundingClientRect();
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <= (window.innerWidth || document.documentElement.clientWidth)   
+
+  );
+};
+
+window.addEventListener('scroll', function() {
+  sections.forEach(section => {
+    if (sectionInViewport(section)) {
+      const sectionHeight = section.getBoundingClientRect().height;
+      const viewportHeight = window.innerHeight;
+      const sectionInViewPercentage = (sectionHeight / viewportHeight) * 100;
+
+      if (sectionInViewPercentage >= 50) {
+        section.classList.add("your-active-class");
+        const navItem = document.querySelector(`nav a[href="#${section.id}"]`);
+        navItem.classList.add("active");
+      }
+    } else {
+      section.classList.remove("your-active-class");
+      const navItem = document.querySelector(`nav a[href="#${section.id}"]`);
+        navItem.classList.remove("active");
+    }
+  });
+}, );
+
